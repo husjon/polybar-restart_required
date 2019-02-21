a4f7b60123456b697b7f149a1a8c0166b042ce0e
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2019-02-21 17:00:58 +0100

    initial

---
+++ restart_required.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /etc/lsb-release
+
+RUNNING_KERNEL=$(uname -r)
+
+case $DISTRIB_ID in
+    Arch)
+        INSTALLED=$(pacman -Q linux | sed 's/linux //' | sed 's/.arch/-arch/');;
+    *)
+        echo Unknown distro
+        exit 1;;
+esac
+
+if [[ ${INSTALLED} != "${RUNNING_KERNEL}" ]]; then
+    echo "Reboot required"
+    exit 0
+else
+    echo ""
+fi
